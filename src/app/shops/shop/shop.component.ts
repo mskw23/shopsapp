faa129a6d2012539126d9ddcf94d64125752c556
mskwierczynski <mateusz@MBP-Mateusz.home>
2018-01-13 17:06:38 +0100

    shop component modified

--- src/app/shops/shop/shop.component.ts
+++ src/app/shops/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+
+  @Input()
+  imgSrc: string
+
+  getImgSrc() {
+    return this.imgSrc;
+  }
 
   constructor() { }
 
